c7635165505ec4b4e7b2bf7637a537205e8f024e
Алексей Горшков <azzz15398@gmail.com>
2026-04-09 19:03:45 +0500

    добавлены методы сложения, вычитания, умножения

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
 using System;
+
+0 references
 class Program
 {
-    static void Main(string[] args)
-    {
-        // VERSION: 1.0.0
-        // Здесь будет логика калькулятора
-    }
+0 references
+static int Add(int a, int b)
+{return a + b; }
+
+O references
+static int Subtract(int a, int b)
+{ return a - b; }
+
+0 references
+static int Multiply(int a, int b)
+{ return a * b; }
+
+O references
+static void Main(string[] args)
+{
+// VERSION: 1.0.0
+// Здесь будет логика калькулятора
+}
 }
